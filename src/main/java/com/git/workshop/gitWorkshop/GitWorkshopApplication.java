685ccff4078a463f9b057ad02d09dcc27fe1c488
Pawel Janusz <external.pawel.janusz@kuehne-nagel.com>
2024-01-30 17:24:41 +0100

    First commit

---
+++ src/main/java/com/git/workshop/gitWorkshop/GitWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.git.workshop.gitWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/workshop/gitWorkshop/GitWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.workshop.gitWorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
